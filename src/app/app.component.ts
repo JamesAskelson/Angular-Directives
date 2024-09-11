07f257f3a78505a5274463a5b1e3ca43debdb6fa
JamesAskelson <jamesaskelson630@gmail.com>
2024-09-11 15:34:28 -0500

    angular-directive-selective

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -13,4 +14,30 @@ export class AppComponent {
   title = 'Directives-Learning';
   isLoggedIn:boolean = true;
   userName: string = 'John Doe';
+  isGuest: boolean = true;
+  isAdmin: boolean = false;
+  isModerator: boolean = false;
+  loginCount: number = 0;
+
+  makeAdmin(){
+    this.isAdmin = true;
+    this.isModerator = false;
+    this.isGuest = false;
+  }
+
+  makeModerator(){
+    this.isAdmin = false;
+    this.isModerator = true;
+    this.isGuest = false;
+  }
+
+  makeGuest(){
+    this.isAdmin = false;
+    this.isModerator = false;
+    this.isGuest = true;
+  }
+
+  loginAttempt(){
+    this.loginCount ++;
+  }
 }
